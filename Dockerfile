fb80303e743404d100c7a20d613a1858206764b6
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-10 14:11:32 +0000

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+
+
+FROM debian:latest
+MAINTAINER Regner Blok-Andersen <shadowdf@gmail.com>
+
+RUN apt-get update -qq
+RUN apt-get upgrade -y -qq
+RUN apt-get install -y -qq python-dev python-pip
+RUN pip install -qU pip
+
+ENV GOOGLE_APPLICATION_CREDENTIALS "path-to-credentials.json"
+ENV GCLOUD_DATASET_ID "your gce project"
+
+ADD en_notifications.py /en_notifications/
+ADD requirements.txt /en_notifications/
+
+WORKDIR /en_notifications/
+
+RUN pip install -r requirements.txt
+
+CMD python /en_notifications/en_notifications.py
---
+++ en_notifications.py
@@ -0,0 +1,46 @@
+
+
+import os
+import logging
+
+from time import sleep
+from gcloud import datastore, pubsub
+
+logging.basicConfig()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# App Settings                                                                                                                                        
+SLEEP_TIME = int(os.environ.get('SLEEP_TIME', 10))
+PULL_COUNT = int(os.environ.get('PULL_COUNT', 1))
+
+# Datastore Settings
+DS_CLIENT = datastore.Client()
+SETTINGS_KIND = os.environ.get('DATASTORE_SETTINGS_KIND', 'EN-SETTINGS')
+
+# PubSub Settings
+PS_CLIENT = pubsub.Client()
+PS_TOPIC = PS_CLIENT.topic(os.environ.get('NOTIFICATION_TOPIC', 'send_notification'))
+
+if not PS_TOPIC.exists():
+    PS_TOPIC.create()
+
+SUBSCRIPTION = PS_TOPIC.subscription('en_notifications')
+
+
+while True:
+    logger.info('Polling for new notifications...')
+    received = PS_SUBSCRIPTION.pull(max_messages=PULL_COUNT)
+    ack_ids = []
+    
+    for message in received:
+        try:
+            logger.info('Got message ID {} with data {}.'.format(message[1].message_id, message[1].data)
+            ack_ids.append(message[0])
+        
+        except:
+            logger.info('Something went wrong with message ID {}.'.format(message[1].message_id))
+    
+    PS_SUBSCRIPTION.acknowledge(ack_ids)
+
+    sleep(SLEEP_TIME)
