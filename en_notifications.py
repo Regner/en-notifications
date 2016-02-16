005b6d8750ae9851835aa237363fbe27c38fa570
Regner Blok-Andersen <shadowdf@gmail.com>
2016-02-16 08:43:59 +0000

    starting work on implementing GCM

--- en_notifications.py
+++ en_notifications.py
@@ -1,8 +1,10 @@
 
 
 import os
+import json
 import logging
 
+from gcm import GCM
 from time import sleep
 from gcloud import datastore, pubsub
 
@@ -11,7 +13,7 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 # App Settings                                                                                                                                        
-SLEEP_TIME = int(os.environ.get('SLEEP_TIME', 60))
+DEFAULT_SLEEP_TIME = int(os.environ.get('SLEEP_TIME', 60))
 PULL_COUNT = int(os.environ.get('PULL_COUNT', 1))
 
 # Datastore Settings
@@ -34,16 +36,40 @@ if not PS_SUBSCRIPTION.exists():
 while True:
     logger.info('Polling for new notifications...')
     received = PS_SUBSCRIPTION.pull(max_messages=PULL_COUNT)
+    sleep_time = DEFAULT_SLEEP_TIME
     ack_ids = []
     
     for message in received:
-        try:
-            logger.info('Got message ID {} with data {}.'.format(message[1].message_id, message[1].data))
-            ack_ids.append(message[0])
+        logger.info('Got message ID {} with data {}.'.format(message[1].message_id, message[1].data))
         
-        except:
-            logger.info('Something went wrong with message ID {}.'.format(message[1].message_id))
+        title = message[1].data
+        url = message[1].attributes['url']
+        feed_name = message[1].attributes['feed_name']
+        service = message[1].attributes['service']
+        collapse_key = message[1].attributes['collapse_key']
+        character_ids = json.loads(message[1].attributes['character_ids'])
+        
+        # Get registration IDs
+        
+        notification = {
+            'title': title,
+            'url': url,
+            'feed_name': feed_name,
+        }
+        
+        # response = gcm.json_request(
+        #     registration_ids=registration_ids,
+        #     data=notification,
+        #     collapse_key=collapse_key,
+        # )
+        
+        # Update registration IDs
+        # Remove registration IDs
+        
+        ack_ids.append(message[0])
     
+    if len(ack_ids) > 0:
         PS_SUBSCRIPTION.acknowledge(ack_ids)
-
-    sleep(SLEEP_TIME)
+        sleep_time = 0
+    
+    sleep(sleep_time)
